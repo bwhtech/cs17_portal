7603a18ce7899958d07d1c12406cdb154f79f3f6
Isha Dewangan <ishadewangan055@gmail.com>
2026-05-20 13:06:45 +0530

    feat: Add CS17 Assignment Submission doctype and related files

---
+++ cs17_portal/cs17_portal/doctype/cs17_assignment_submission/cs17_assignment_submission.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, developers@bwh.tech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("CS17 Assignment Submission", {
+// 	refresh(frm) {
+
+// 	},
+// });
