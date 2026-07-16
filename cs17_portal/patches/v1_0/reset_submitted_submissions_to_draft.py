3884084b346c5ad7f50efc4a4d09fc29acf751fe
Isha Dewangan <ishadewangan055@gmail.com>
2026-07-16 20:25:16 +0530

    refactor: streamline code formatting in test and patch files

--- cs17_portal/patches/v1_0/reset_submitted_submissions_to_draft.py
+++ cs17_portal/patches/v1_0/reset_submitted_submissions_to_draft.py
@@ -9,10 +9,6 @@ import frappe
 
 
 def execute():
-	submitted = frappe.get_all(
-		"CS17 Assignment Submission", filters={"docstatus": 1}, pluck="name"
-	)
+	submitted = frappe.get_all("CS17 Assignment Submission", filters={"docstatus": 1}, pluck="name")
 	for name in submitted:
-		frappe.db.set_value(
-			"CS17 Assignment Submission", name, "docstatus", 0, update_modified=False
-		)
+		frappe.db.set_value("CS17 Assignment Submission", name, "docstatus", 0, update_modified=False)
